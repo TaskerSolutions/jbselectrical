1e340ca23bf8fb665adc28e47ee0790b5e4b03b5
TaskerSolutions <79877715+TaskerSolutions@users.noreply.github.com>
2021-04-06 18:48:39 +1200

    1

--- script/script.js
+++ script/script.js
@@ -3,9 +3,38 @@ $('.navbar-nav>li>a').on('click', function(){
   $('.navbar-collapse').collapse('hide');
 });
 
+var overlayHidden = true;
+
 $(document).ready(function () {
   // animate nav bar burger icon to close icon
   $('.navbar-toggler').on('click', function () {
     $('.animated-icon').toggleClass('open');
+    
+    var overlay = $('.overlay');
+    var navbar = $('.navbar');
+    
+    if(overlayHidden) {
+      // show overlay
+      overlayHidden = false;
+      overlay.show();
+      overlay.animate(
+          {opacity: "1"},
+          200
+        );
+      overlay.css("z-index", "20");
+      navbar.css("background-color", "#00000000");
+    } else {
+      // hide overlay
+      overlayHidden = true;
+      overlay.animate(
+        {opacity: "0"},
+        200
+      );
+      setTimeout(() => {
+         overlay.css("z-index", "-20")
+         navbar.css("background-color", "#1b1b1be8");
+      }, 200);
+     
+    }
   });
 });
